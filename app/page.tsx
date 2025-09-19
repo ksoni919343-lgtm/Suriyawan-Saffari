f48eeccd2daec1bf625dc3895c1af2ebaf197469
ksoni919343-lgtm <ksoni919343@gmail.com>
2025-09-19 19:16:29 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl text-warmBrown">Welcome to Suriyawan Saffari</h1>
+    </div>
+  );
+}
