cac6754b5003435b017d192af9de887a54eed055
ksoni919343-lgtm <ksoni919343@gmail.com>
2025-09-19 19:29:31 +0530

    Create ErrorBoundary.tsx

---
+++ components/ErrorBoundary.tsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+export default class ErrorBoundary extends Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <p>Something went wrong.</p>;
+    }
+    return this.props.children;
+  }
+}
