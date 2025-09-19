4b1d2524bc881f2c78c9c0c58ca95506a89a59bd
ksoni919343-lgtm <ksoni919343@gmail.com>
2025-09-19 19:34:41 +0530

    Create GooglePicker.tsx

---
+++ components/GooglePicker.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { useEffect } from 'react';
+
+export default function GooglePicker({ onPick }) {
+  useEffect(() => {
+    // Load Google Picker API
+  }, []);
+
+  return <button onClick={() => {}}>Pick File</button>;
+}
