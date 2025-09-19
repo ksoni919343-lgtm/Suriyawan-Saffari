c2485b85191e4cebfe6524a793c3e78f9f5c9c42
ksoni919343-lgtm <ksoni919343@gmail.com>
2025-09-19 21:42:42 +0530

    Create index.ts

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+  description: string;
+  category_id: string;
+  seller_id: string;
+}
+
+// Other interfaces
