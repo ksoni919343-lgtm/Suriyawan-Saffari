2bfbfb92260b0cb6a5fd7178bd4cda11099b181f
ksoni919343-lgtm <ksoni919343@gmail.com>
2025-09-20 16:47:19 +0530

    Update HelpChat.tsx

--- components/HelpChat.tsx
+++ components/HelpChat.tsx
@@ -1,5 +1,5 @@
 'use client';
-import Crisp from '@crisp/chatbox';
+import Crisp from 'crisp-sdk-web'; // सही इंपोर्ट, @crisp/chatbox को हटाया
 
 export default function HelpChat() {
   return <Crisp websiteId={process.env.CRISP_WEBSITE_ID} />;
